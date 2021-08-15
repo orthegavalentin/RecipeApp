5a580399eb541fddc05629c3c87030cbfbc5683e
orthegavalentin <iravadu@gmail.com>
2021-08-15 13:24:34 +0200

    basic setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('test1', 'a test recipe',
+    'https://media-cdn.tripadvisor.com/media/photo-o/13/a4/cc/3d/poulet-tikka-poulet-tikka.jpg'), new Recipe('test1', 'a test recipe',
+      'https://media-cdn.tripadvisor.com/media/photo-o/13/a4/cc/3d/poulet-tikka-poulet-tikka.jpg')]
 
   constructor() { }
 
